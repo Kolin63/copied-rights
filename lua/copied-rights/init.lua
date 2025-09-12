41800a5dcb2b8ee834310f970061b8b983729bf6
Kolin63 <theverylastgoodname@gmail.com>
2025-09-12 01:12:18 -0400

    Add add_header Function

--- lua/copied-rights/init.lua
+++ lua/copied-rights/init.lua
@@ -20,9 +20,45 @@ M.debug = function()
   end
 end
 
+-- removes any extra syntax from file name
+-- ex: *.c -> c
+function clean_file_name(file)
+  if file:sub(1, 1) == "*" then file = file:sub(2) end
+  if file:sub(1, 1) == "." then file = file:sub(2) end
+  return file
+end
+
+-- neatly adds header to config
+-- does not check for file type conflicts
+function add_header(header)
+  if header.file ~= nil then
+    header.file = clean_file_name(header.file)
+  else
+    -- without a file type, it is invalid
+    print("Copied Rights: ERROR: Header must include a file type")
+    goto exception
+  end
+
+  if header.lines ~= nil then
+    -- lines must be a table
+    if type(header.lines) ~= "table" then
+      header.lines = { header.lines }
+    end
+  else
+    -- without lines, it is invalid
+    print("Copied Rights: ERROR: Header must include lines")
+    goto exception
+  end
+
+  table.insert(config.headers, header)
+  ::exception::
+end
+
 -- setup config
 M.setup = function(input)
-  if input.headers ~= nil then config.headers = input.headers end
+  if input.headers ~= nil then
+    for _, header in ipairs(input.headers) do add_header(header) end
+  end
   if input.search_stop ~= nil then config.search_stop = input.search_stop end
   if input.max_search ~= nil then config.max_search = input.max_search end
 
@@ -41,13 +77,23 @@ M.setup = function(input)
       -- otherwise, inserts at end
       for _, i in ipairs(file_input) do
         for ji, j in ipairs(config.headers) do
+
+          -- without a file type, it is invalid
+          if i.file == nil then
+            print("Copied Rights: ERROR: Header must include a file type")
+            goto header_replaced
+          end
+
+          -- remove any extra syntax
+          i.file = clean_file_name(i.file)
+
           if i.file == j.file then
             table.remove(config.headers, ji)
-            table.insert(config.headers, ji, i)
+            add_header(i)
             goto header_replaced
           end
         end
-        table.insert(config.headers, i)
+        add_header(i)
         ::header_replaced::
       end
     end
